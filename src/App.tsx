50613069f0088e5e07ba2312a6ac17cc5e9c363b
T-unity <takadanaritoshi@gmail.com>
2021-12-15 22:14:38 +0900

    localhost3000からlocalhost8000にアクセスできる事を確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './index.css';
 
+const path: string = 'http://localhost:8000/api/tweet/';
+fetch(path).then(res =>
+  console.log(res.json())
+  );
+
 function App() {
   return (
     <div className="start-project">

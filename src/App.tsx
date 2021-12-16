e76b49aa954c0820f3b94608ced620626a258b28
T-unity <takadanaritoshi@gmail.com>
2021-12-16 23:07:19 +0900

    async/awaitでデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './index.css';
 
 const path: string = 'http://localhost:8000/api/tweet/';
-fetch(path).then(res =>
-  console.log(res.json())
-  );
+// fetch(path).then(res =>
+//   console.log(res.json())
+// );
+
+const getData = async () => {
+  const res = await fetch(path);
+  console.log(res.json());
+  // const { results } = await res.json();
+  // console.log(results);
+}
+
+getData();
 
 function App() {
   return (

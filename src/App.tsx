82939b7093254afc15155f0187248ea23418974d
T-unity <takadanaritoshi@gmail.com>
2021-12-17 21:55:08 +0900

    APIサーバーからデータを取得するためのコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import APIfetch from 'components/APIfetch';
 import React from 'react';
 import './index.css';
 
 function App() {
   return (
     <div className="start-project">
+      <APIfetch />
     </div>
   );
 }

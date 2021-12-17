4116a8694260de4c2759d682e1b2b6e99a22a688
T-unity <takadanaritoshi@gmail.com>
2021-12-17 21:50:11 +0900

    不要なコードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import './index.css';
 
-const path: string = 'http://localhost:8000/api/tweet/';
-// fetch(path).then(res =>
-//   console.log(res.json())
-// );
-
-const getData = async () => {
-  // const res = await fetch(path);
-  const res = await (await fetch(path)).json();
-  console.log(res);
-  // console.log(res.json());
-  // const { results } = await res.json();
-  // console.log(results);
-}
-
-getData();
-
 function App() {
   return (
     <div className="start-project">
-      <h1>伝説が始まる...</h1>
     </div>
   );
 }
